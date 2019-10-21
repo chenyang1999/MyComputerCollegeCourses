3c74ade1fce072c44ded1d382e4332a2966e87c6
chenyang1999 <34475230+chenyang1999@users.noreply.github.com>
2019-10-21 19:40:46 +0800

    Update a05q02.py

--- CS116课后题/05/Interface/a05q02.py
+++ CS116课后题/05/Interface/a05q02.py
@@ -1,9 +1,6 @@
 import math
-Lp=[0]*10
-
 import sys
 sys.setrecursionlimit(2000)
-
 def highest(n):
   if n>=10:
     n=n//10
@@ -12,7 +9,7 @@ def highest(n):
   
 def gen_pd(i):
   return math.log10((i+1)/i)
-
+  
 def gen_a(L:list,i):
   if i in L:
     return L.count(i)
@@ -25,16 +22,15 @@ def solve(p,a,n):
     
 def obey_benford(L):
   #YOUR CODE GOES HERE
-  p=list(map(gen_pd,range(1,10)))
+  p=list(map(lambda i:math.log10((i+1)/i),range(1,10)))
 #  print(p)
   L=list(map(highest, L))
 #  print(L)
   n=len(L)
-  a=list(map(gen_a,[L]*10,range(1,10)))
+  a=list(map(lambda L,i:L.count(i),[L]*9,range(1,10)))
 #  print(a)
-  ans=list(map(solve,p,a,[n]*10))
+  ans=sum(list(map(lambda p,a,n:math.pow(a-n*p, 2)/(n*p),p,a,[n]*9)))
 #  print(ans)
-  ans=sum(ans)
   return ans
 
 
